ecbcc94cbc72e53e817e4e16328b81ca371587e0
Thiago Gonzalez <contato@thiagogonzalez.com>
2023-11-09 15:01:11 -0300

    Create a new support folder

--- build/web/themes/algemetric/tokens.cjs.d.ts
+++ build/web/themes/algemetric/tokens.cjs.d.ts
@@ -397,19 +397,21 @@ declare const tokens: {
     "table": {
       "background": DesignToken
     },
-    "svg-gradient": {
-      "background": {
-        "1": {
-          "start": DesignToken,
-          "end": DesignToken
-        },
-        "2": {
-          "start": DesignToken,
-          "end": DesignToken
-        },
-        "3": {
-          "start": DesignToken,
-          "end": DesignToken
+    "support": {
+      "login-gradient": {
+        "background": {
+          "1": {
+            "start": DesignToken,
+            "end": DesignToken
+          },
+          "2": {
+            "start": DesignToken,
+            "end": DesignToken
+          },
+          "3": {
+            "start": DesignToken,
+            "end": DesignToken
+          }
         }
       }
     },
